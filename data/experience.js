fcd1cfaa7eaf3fb1e8e879640c3231eefb9ef590
aaronbolival <41184160+AaronBolival@users.noreply.github.com>
2026-07-25 01:42:28 +0800

    Add files via upload

--- data/experience.js
+++ data/experience.js
@@ -1,1 +1,525 @@
+const experienceData = [
+    {
+        id: "0",
+        jobTitle:"SAP ABAP Developer",
+        jobDescription: "",
+        jobLevel: "12",
+        company:"DXC Technology",
+        officeLocation: "IPC Taguig / Eton Centris",
+        startDate: "2021-11-08",
+        endDate: null,
+        status: "Employed",
+        employmentType: "Fulltime | Regular",
+        tech: "SAP",
+        clientDetails: [ 
+            {
+                clientName: [
+                    { text: "Client:", hidden: "false", type: "label" },
+                    {
+                        text: "WEC Energy Group Inc.",
+                        hidden: "false",
+                        type: "content"
+                    }
+                ],
+                clientExperience : [ 
+                    { text: "Responsibilities", hidden: "false", type: "label" },
+                    {
+                        text: "Work with FI, MM, PM, BW, PI/PO, HR, Basis and other functional consultants to work on new projects and to identify the cause of existing and new program issues of the system.",
+                        hidden: "false",
+                        type: "content"
+                    },
+                    {
+                        text: "Identify the root cause of issue related to oData, integrations, standard/custom programs and apply a fix if needed.",
+                        hidden: "false",
+                        type: "content"
+                    },
+                    {
+                        text: "Work with fiori developer to identify the cause of issue of the app.",
+                        hidden: "false",
+                        type: "content"
+                    },
+                    {
+                        text: "Modify the existing objects/programs such as reports, forms (Smartforms, SAPScript) OData programs, and other custom programs.",
+                        hidden: "false",
+                        type: "content"
+                    },
+                    {
+                        text: "Debug the standard and custom programs and enhance the program if needed.",
+                        hidden: "false",
+                        type: "content"
+                    },
+                    {
+                        text: "Creating and provide technical analysis, estimates, technical unit test and code review documents related to the issues, fix/solution and programs.",
+                        hidden: "false",
+                        type: "content"
+                    }                                               
+                ],
+                clientModule: [
+                    { text: "Client SAP Modules :", hidden: "false", type: "label" },
+                    { text: "FICO", hidden: "false", type: "content" },
+                    { text: "MM", hidden: "false", type: "content" },
+                    { text: "PM", hidden: "false", type: "content" },
+                    { text: "Fiori", hidden: "false", type: "content" },
+                    { text: "Basis", hidden: "false", type: "content" },
+                    { text: "PI/PO", hidden: "false", type: "content" },
+                    { text: "BPM", hidden: "false", type: "content" },
+                    { text: "HR", hidden: "false", type: "content" },
+                    { text: "BW", hidden: "false", type: "content" }
 
+                ],
+                achievements: [
+                    { text: "", hidden: "true", type: "label" },
+                    { text: "", hidden: "true", type: "content" }               
+                ],  
+                clientLocation:"US",
+                status: "Active",
+                startDate: "2024-05-02",
+                endDate: null,
+                clientType: "AMS ( Application Management Support )",
+                employmentType : "Fulltime | Regular"
+            } 
+        ],
+        modules: [
+            "","",""
+        ],
+        experiences : [
+            "",
+            "",
+            "",
+            ""
+        ],
+        otherSharable : [
+            "",
+            "",
+            ""
+        ],
+        achievements : [
+            "",
+            "",
+            ""
+        ],
+        hidden: "false"
+    },
+    {
+        id: "1",
+        jobTitle:"SAP ABAP Developer",
+        jobDescription: "",
+        jobLevel: "",
+        company:"DXC Technology",
+        officeLocation: "IPC Taguig / Eton Centris",
+        startDate: "2021-11-08",
+        endDate: null,
+        status: "Inactive",
+        employmentType: "Fulltime | Regular",
+        tech: "SAP",
+        clientDetails: [ 
+            {
+                clientName: [
+                    { text: "Client:", hidden: "false", type: "label" },
+                    {
+                        text: "Del Monte Philippines",
+                        hidden: "false",
+                        type: "content"
+                    },
+                    {
+                        text: "Nutri Asia Inc.",
+                        hidden: "false",
+                        type: "content"
+                    }
+                ],
+                clientExperience : [ 
+                    { text: "Responsibilities", hidden: "false", type: "label" },
+                    {
+                        text: "Work with FI, MM, SD functional consultants to work on new projects and to identify the cause of existing and new program issues of the system",
+                        hidden: "false",
+                        type: "content"
+                    },
+                    {
+                        text: "Create an upload program using BAPI",
+                        hidden: "false",
+                        type: "content"
+                    },
+                    {
+                        text: "Copy and modify the existing objects / programs such as Report program, Forms(Smartforms, SAPScript) and other custom program",
+                        hidden: "false",
+                        type: "content"
+                    },
+                    {
+                        text: "Enhancing the standard program using BADI, exits and implicit enhancement",
+                        hidden: "false",
+                        type: "content"
+                    },
+                    {
+                        text: "Debug the standard and custom programs and solve/fix the program/system issue",
+                        hidden: "false",
+                        type: "content"
+                    },
+                    {
+                        text: "Testing the created program / modified programs",
+                        hidden: "false",
+                        type: "content"
+                    }                                               
+                ],
+                clientModule: [
+                    { text: "Client SAP Modules :", hidden: "false", type: "label" },
+                    { text: "FICO", hidden: "false", type: "content" },
+                    { text: "MM", hidden: "false", type: "content" },
+                    { text: "SD", hidden: "false", type: "content" },
+                    { text: "Basis", hidden: "false", type: "content" }
+                ],
+                achievements: [
+                    { text: "", hidden: "false", type: "label" },
+                    { text: "", hidden: "false", type: "content" }               
+                ],  
+                clientLocation:"PH",
+                status: "Moved to another account/client",
+                startDate: "2022-11-17",
+                endDate: "2024-03-31",
+                clientType: "AMS ( Application Management Support )",
+                employmentType : "Fulltime | Regular"
+            } 
+        ],
+        modules: [
+            "","",""
+        ],
+        experiences : [
+            "",
+            "",
+            "",
+            ""
+        ],
+        otherSharable : [
+            "",
+            "",
+            ""
+        ],
+        achievements : [
+            "",
+            "",
+            ""
+        ],
+        hidden: "false"
+    },
+    {
+        id: "2",
+        jobTitle:"SAP ABAP Developer",
+        jobDescription: "",
+        jobLevel: "",
+        company:"DXC Technology",
+        officeLocation: "IPC Taguig / Eton Centris",
+        startDate: "2021-11-08",
+        endDate: null,
+        status: "Active",
+        employmentType: "Fulltime | Regular",
+        tech: "SAP",
+        clientDetails: [ 
+            {
+                clientName: [
+                    { text: "Client:", hidden: "false", type: "label" },                    
+                    {
+                        text: "Pilmico",
+                        hidden: "false",
+                        type: "content"
+                    }
+                ],
+                clientExperience : [ 
+                    { text: "Responsibilities", hidden: "false", type: "label" },
+                    {
+                        text: "Develop Adobe Forms, ALV Reports, creating CDS views and upload programs with BAPI,",
+                        hidden: "false",
+                        type: "content"
+                    },
+                    {
+                        text: "Creating technical specification and unit test documentation for the created ABAP program.",
+                        hidden: "false",
+                        type: "content"
+                    },
+                    {
+                        text: "Providing support to teammates in the creation of technical specification to reduce their workload",
+                        hidden: "false",
+                        type: "content"
+                    }                                    
+                ],
+                clientModule: [
+                    { text: "Client SAP Modules :", hidden: "false", type: "label" },
+                    { text: "FICO", hidden: "false", type: "content" },
+                    { text: "MM", hidden: "false", type: "content" },
+                    { text: "SD", hidden: "false", type: "content" },
+                    { text: "TM", hidden: "false", type: "content" }
+                ],
+                achievements: [
+                    { text: "", hidden: "false", type: "label" },
+                    { text: "", hidden: "false", type: "content" }               
+                ],  
+                clientLocation:"PH",
+                status: "Moved to another account/client",
+                startDate: "2022-07-08",
+                endDate: "2022-09-30",
+                clientType: "Implementation",
+                employmentType : "Fulltime | Regular"
+            } 
+        ],
+        modules: [
+            "","",""
+        ],
+        experiences : [
+            "",
+            "",
+            "",
+            ""
+        ],
+        otherSharable : [
+            "",
+            "",
+            ""
+        ],
+        achievements : [
+            "",
+            "",
+            ""
+        ],
+        hidden: "false"
+    },
+    {
+        id: "3",
+        jobTitle:"Power Platform Developer (Volunteer)",
+        jobDescription: "",
+        jobLevel: "",
+        company:"DXC Technology",
+        officeLocation: "IPC Taguig / Eton Centris",
+        startDate: "2021-11-08",
+        endDate: null,
+        status: "Inactive",
+        employmentType: "Fulltime | Regular",
+        tech: "Power Platform",
+        clientDetails: [ 
+            {
+                clientName: [
+                    { text: "Client:", hidden: "false", type: "label" },
+                    {
+                        text: "DXC Employees and Manager",
+                        hidden: "false",
+                        type: "content"
+                    }
+                ],
+                clientExperience : [ 
+                    { text: "Responsibilities", hidden: "false", type: "label" },
+                    {
+                        text: "Helping to develop DXC application using Microsoft Power Platform such as Power Apps, Power BI, Power Automate",
+                        hidden: "false",
+                        type: "content"
+                    },
+                    {
+                        text: "Maintain the application and records in sharepoint",
+                        hidden: "false",
+                        type: "content"
+                    }                               
+                ],
+                clientModule: [
+                    { text: "", hidden: "true", type: "label" },
+                    { text: "", hidden: "true", type: "content" }
+
+                ],
+                achievements: [
+                    { text: "", hidden: "false", type: "label" },
+                    { text: "", hidden: "false", type: "content" }               
+                ],  
+                clientLocation:"PH",
+                status: "Moved to another account/client",
+                startDate: "2022-06-07",
+                endDate: "2022-08-05",
+                clientType: "Implementation",
+                employmentType : "Volunteer"
+            } 
+        ],
+        modules: [
+            "","",""
+        ],
+        experiences : [
+            "",
+            "",
+            "",
+            ""
+        ],
+        otherSharable : [
+            "",
+            "",
+            ""
+        ],
+        achievements : [
+            "",
+            "",
+            ""
+        ],
+        hidden: "false"
+    },
+    {
+        id: "4",
+        jobTitle:"Data Entry Specialist",
+        jobDescription: "",
+        jobLevel: "",
+        company:"DXC Technology",
+        officeLocation: "IPC Taguig / Eton Centris",
+        startDate: "2021-11-08",
+        endDate: null,
+        status: "Inactive",
+        employmentType: "Fulltime | Regular",
+        tech: "N/A",
+        clientDetails: [ 
+            {
+                clientName: [
+                    { text: "Client:", hidden: "false", type: "label" },
+                    {
+                        text: "Procter and Gamble (P&G)",
+                        hidden: "false",
+                        type: "content"
+                    }
+                ],
+                clientExperience : [ 
+                    { text: "Responsibilities", hidden: "false", type: "label" },
+                    {
+                        text: "Copy the records from P&G systems into SharePoint and Microsoft office such as Word and Excel or vice versa.",
+                        hidden: "false",
+                        type: "content"
+                    },
+                    {
+                        text: "Responsible for consolidation of data or records.",
+                        hidden: "false",
+                        type: "content"
+                    },
+                    {
+                        text: "Update and check the contents of client SharePoint and documents.",
+                        hidden: "false",
+                        type: "content"
+                    },
+                    {
+                        text: "Organize the files and maintain the records in SharePoint.",
+                        hidden: "false",
+                        type: "content"
+                    }                                
+                ],
+                clientModule: [
+                    { text: "", hidden: "true", type: "label" },
+                    { text: "", hidden: "true", type: "content" }
+                ],
+                achievements: [
+                    { text: "Achievements", hidden: "false", type: "label" },
+                    { text: "The project has been extended because of the teams effort and I was one of the selected employee to continue the project", hidden: "false", type: "content" }               
+                ],    
+                clientLocation:"US",
+                status: "Moved to another account/client",
+                startDate: "2021-12-01",
+                endDate: "2022-06-30",
+                clientType: "Implementation",
+                employmentType : "Fulltime | Regular"
+            } 
+        ],
+        modules: [
+            "","",""
+        ],
+        experiences : [
+            "",
+            "",
+            "",
+            ""
+        ],
+        otherSharable : [
+            "",
+            "",
+            ""
+        ],
+        achievements : [
+            "",
+            "",
+            ""
+        ],
+        hidden: "false"
+    },
+    {
+        id: "5",
+        jobTitle:"Accounting Staff",
+        jobDescription: "",
+        jobLevel: "",
+        company:"JNS Seafood Supplier",
+        officeLocation: "Farmers, Quezon City",
+        startDate: "2020-03-11",
+        endDate: "2021-11-14",
+        status: "Resigned",
+        employmentType: "Fulltime | Regular",
+        tech: "N/A",
+        clientDetails: [ 
+            {
+                clientName: [
+                    { text: "Client:", hidden: "true", type: "label" },
+                    {
+                        text: "",
+                        hidden: "false",
+                        type: "content"
+                    }
+                ],
+                clientExperience : [ 
+                    { text: "Responsibilities", hidden: "false", type: "label" },
+                    {
+                        text: "Responsible for data entry and sales report.",
+                        hidden: "false",
+                        type: "content"
+                    },
+                    {
+                        text: "Analyzing the discrepancies of customer payments.",
+                        hidden: "false",
+                        type: "content"
+                    },
+                    {
+                        text: "Supported other tasks such as simple computer trouble shooting, and helping members on their queries about NetSuite.",
+                        hidden: "false",
+                        type: "content"
+                    },
+                    {
+                        text: "Maintain the records of customers.",
+                        hidden: "false",
+                        type: "content"
+                    },
+                    {
+                        text: "Responsible for some accounting works such as Inventory, Bank Reconciliation, Account Receivables and Collections using NetSuite and Quickbooks.",
+                        hidden: "false",
+                        type: "content"
+                    },                                
+                ],
+                clientModule: [
+                    { text: "", hidden: "true", type: "label" },
+                    { text: "", hidden: "true", type: "content" }               
+                ],
+                achievements: [
+                    { text: "Achievements", hidden: "false", type: "label" },
+                    { text: "Awarded for being a team player for leading the accounting team temporarily.", hidden: "false", type: "content" }               
+                ],                
+                clientLocation:"PH",
+                status: "Resigned",
+                startDate: "2020-03-11",
+                endDate: "2021-11-14",
+                clientType: "Implementation",
+                employmentType : "Fulltime | Regular"
+            } 
+        ],
+        modules: [
+            "","",""
+        ],
+        experiences : [
+            "",
+            "",
+            "",
+            ""
+        ],
+        otherSharable : [
+            "",
+            "",
+            ""
+        ],
+        achievements : [
+            "",
+            "",
+            ""
+        ],
+        hidden: "true"
+    }                       
+];
---
+++ data/projects.js
@@ -0,0 +1,50 @@
+const projects = [{
+        id: "0",
+        title: "Simple SAP RAP Business Application with Freestyle SAPUi5",
+        description: "Fullstack app using ABAP RESTful Application Programming Model, CDS, RAP BO for Backend, and Freestyle SAPUI5 for Frontend",
+        desc_list: [
+            "Manage Author and Books using RAP (preview mode only)", 
+            "Order a book using Freestyle SAPUI5 (BTP Trial using Business Application Studio)",
+            "Cancel, Accept, Deliver an order", 
+            "Flexible column layout",
+            "This will trigger action, determination and validation in the behavior implementation"
+        ],
+        notes: [ 
+                "Notes:",
+                "This is a personal app only developed to showcase my SAP ABAP Restful Application Programming Model(RAP) developement skills.",
+                "This app still has some issues, However, I plan to fix it once I have some free time."
+        ],
+        technologies: ["ABAP", "RAP", "CDS", "EML", "Fiori List Report Floorplan", "Freestyle SAPUI5"],
+        platform_and_tools: [ "BTP Trial", "Business Application Studio" ],
+        icon: "bi-window-sidebar",
+        link: "#",
+        github: [ 
+            {
+                id: "0",
+                link: "https://github.com/AaronBolival/-personal_app-rap_ui5-bookshop.git",
+                title: "Frontend Repository"
+            },            
+            {
+                id: "1",
+                link: "https://github.com/AaronBolival/personal_app-rap-bookshop.git",
+                title: "Backend Repository"
+            }
+        ],
+        youtube: [ 
+            {
+                id: "0",
+                url: "https://www.youtube.com/embed/sBCLblxWVEY?si=l-Rl7XPX",
+                title: "SAP Sample Fullstack application using RAP and Freestyle SAPUI5"
+            }  ,            {
+                id: "1",
+                url: "https://www.youtube.com/embed/sBCLblxWVEY?si=l-Rl7XPXFzHLxfi2",
+                title: "SAP Sample Fullstack application using RAP and Freestyle SAPUI5"
+            }  
+        ],           
+        images : [ 
+            "https://raw.githubusercontent.com/AaronBolival/ajeb_portfolio/refs/heads/main/test.png", 
+            "https://raw.githubusercontent.com/AaronBolival/ajeb_portfolio/refs/heads/main/test.png" ,
+            "https://raw.githubusercontent.com/AaronBolival/ajeb_portfolio/refs/heads/main/test.png"
+        ]
+    }
+];
